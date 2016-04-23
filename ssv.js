bc46ca3b5712ca45e39f201e6064b632453bbc5e
Hans Felde <m33pfish@gmail.com>
2016-04-22 19:54:04 -0500

    Update ssv.js

--- ssv.js
+++ ssv.js
@@ -1,1 +1,17 @@
-
+const V2 = function(x, y) {
+  this.x = x
+  this.y = y
+}
+V2.prototype.add = function(v, out) {
+  out.x = this.x + v.x
+  out.y = this.y + v.y
+  return out
+}
+v2.prototype.selfAdd = function(v) {
+  return this.add(v, this)
+}
+V2.scale = function(n, v) {
+  v.x *= n
+  v.y *= n
+  return v
+}
